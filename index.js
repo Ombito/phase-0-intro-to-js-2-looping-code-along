96fc8bad33e621c35082e2612f31ea8c3eadf1a0
ombito <alvinombito09@gmail.com>
2023-07-16 22:34:07 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cardMessages = function writeCards(name, event){
+    for(let i=name; i < name.length; i++){
+        const name = name[i];
+        
+        return `Thank you, ${name}, for the wonderful surprise ${event} gift!`
+    }
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  countDown(writeCards)
